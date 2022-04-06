be00fa64344137fe74e6bd443bd9bac61acea4eb
oberghub <nataponball@hotmail.com>
2022-04-06 22:17:55 +0700

    Create all file

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const pool = require("../config");
+
+router = express.Router();
+
+router.get("/", async function (req, res, next) {
+    return res.render("index")
+});
+
+exports.router = router;
