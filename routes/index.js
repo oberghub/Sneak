a1a8d4e9db6ebc7360e82d0294eabe9cc7d06014
oberghub <73652245+oberghub@users.noreply.github.com>
2022-04-12 02:25:05 +0700

    Delete routes directory

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const pool = require("../config");
-router = express.Router();
-
-router.get("/", async function (req, res, next) {
-    return res.render("index")
-});
-exports.router = router;
