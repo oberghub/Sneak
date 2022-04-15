67d13b6e10ce4e1a1d483b0006105cb6add552b0
Thitiwut85 <73686921+Thitiwut85@users.noreply.github.com>
2022-04-15 15:11:26 +0700

    test

--- server/config.js
+++ server/config.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'mysql',
-  password: '',
+  user: 'root',
+  password: '123456',
   database: 'sneakershop',
   waitForConnections: true,
   connectionLimit: 10,
