de2059d572509d18fc4daf015fef509ee908e58e
ipin0917826334 <71114672+ipin0917826334@users.noreply.github.com>
2022-04-15 18:23:11 +0700

    Add Item from db

--- server/routes/shop.js
+++ server/routes/shop.js
@@ -4,13 +4,15 @@ const pool = require("../config");
 router = express.Router();
 
 router.get("/shop", async function (req, res, next) {
-    // Your code here
-    console.log("kuy")
-    res.render('shop')
-  });
+  try {
+    const [rows, fields] = await pool.query('SELECT * FROM item')
+  } catch (err) {
+    return next(err);
+  }
+});
 router.post("/shop", async function (req, res, next) {
-    // Your code here
-    console.log("kuy1")
-    res.render('shop')
-  });
+  // Your code here
+  console.log("kuy1")
+  res.render('shop')
+});
 exports.router = router;
